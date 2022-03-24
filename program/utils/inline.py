35dcf1776c988a58bded547e242e60e2db8f72c3
/Oꜰꜰʟɪɴᴇ ‌[ᴀꜰᴋ]「™ʟᴏɢ✓ᴏᴜᴛ❥︎✔︎ <94526115+Unknownvip@users.noreply.github.com>
2022-03-24 13:51:44 +0530

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -21,6 +21,25 @@ def stream_markup(user_id):
     ],
     [
       InlineKeyboardButton(text="🔙 Go Back", callback_data='stream_home_panel'),
+    ],
+  ]
+  return buttons
+
+
+def menu_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="⏹", callback_data=f'set_stop | {user_id}'),
+      InlineKeyboardButton(text="⏸", callback_data=f'set_pause | {user_id}'),
+      InlineKeyboardButton(text="▶️", callback_data=f'set_resume | {user_id}'),
+      InlineKeyboardButton(text="⏭", callback_data=f'set_skip | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🔇", callback_data=f'set_mute | {user_id}'),
+      InlineKeyboardButton(text="🔊", callback_data=f'set_unmute | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🔙 Go Back", callback_data='stream_home_panel'),
     ]
   ]
   return buttons
